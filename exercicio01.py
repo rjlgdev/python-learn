a83f05b71c00f9a09015ef14b582d6bceabd7fbe
rjlgdev <rjgldev@gmail.com>
2021-04-26 21:13:28 -0300

    primeiro commit

---
+++ exercicio01.py
@@ -0,0 +1,4 @@
+print("Olá, mundo")
+
+mensagem = "Olá, mundo 2"
+print(mensagem)
---
+++ exercicio02.py
@@ -0,0 +1,5 @@
+#nome = input("Qual seu nome ? ")     # 1 linha e 2 linha estão comentadas
+#print("É um prazer te conhecer,",nome,"!")
+
+nome = input("Digite seu nome : ")
+print("É um prazer te conhecer {} !".format(nome))
